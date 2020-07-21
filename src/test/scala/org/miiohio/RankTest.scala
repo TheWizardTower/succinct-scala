0154f49d4e3f7810f29570c1ee641ab8a7e3259c
Adam McCullough <merlinfmct87@gmail.com>
2020-07-21 05:06:45 +0000

    Hoo boy, this is messy. But it passes test. Please refactor.
    It's ugly, there's duplicated code, and I haven't tested it thoroughly.
    
    But it works! Even for non-byte-aligned arrays!

--- src/test/scala/org/miiohio/RankTest.scala
+++ src/test/scala/org/miiohio/RankTest.scala
@@ -2,6 +2,7 @@ package org.miiohio
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.miiohio.Rank
+import scodec.bits._
 
 class RankTest extends AnyFlatSpec {
 
@@ -20,6 +21,18 @@ class RankTest extends AnyFlatSpec {
       assert(Rank.countOnesInByteUpToIndex(-128, 0) == 1)
       assert(Rank.countOnesInByteUpToIndex(-1, 0) == 1)
       assert(Rank.countOnesInByteUpToIndex(-1, 1) == 2)
+  }
+
+  "populateCache" should "Count correctly." in {
+      assert(Rank.populateCache(hex"00000000".toBitVector, 4) == Vector(RankCache(0, Vector(0,0,0,0))))
+      assert(Rank.populateCache(hex"0000000000000000".toBitVector, 4) == Vector(RankCache(0, Vector(0,0,0,0)), RankCache(0, Vector(0,0,0,0))))
+      assert(Rank.populateCache(hex"deadbeef".toBitVector, 4) == Vector(RankCache(0, Vector(6,5,6,7))))
+      assert(Rank.populateCache(hex"deadbabe".toBitVector, 4) == Vector(RankCache(0, Vector(6,5,5,6))))
+      assert(Rank.populateCache(hex"deadbeefdeadbabe".toBitVector, 4) == Vector(RankCache(0, Vector(6,5,6,7)), RankCache(24, Vector(6,5,5,6))))
+  }
+
+  "populateCache" should "Handle not-byte-aligned arrays." in {
+      assert(Rank.populateCache(bin"00", 4) == Vector(RankCache(0, Vector(0))))
   }
 
 }
