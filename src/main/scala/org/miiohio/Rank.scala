442e0f0abd771823fd6519151de3e5852cca5a6c
Adam McCullough <merlinfmct87@gmail.com>
2020-07-21 05:06:45 +0000

    Hoo boy, this is messy. But it passes test. Please refactor.
    It's ugly, there's duplicated code, and I haven't tested it thoroughly.
    
    But it works! Even for non-byte-aligned arrays!

--- src/main/scala/org/miiohio/Rank.scala
+++ src/main/scala/org/miiohio/Rank.scala
@@ -3,17 +3,14 @@ package org.miiohio
 import scodec.bits._
 
 case class RankCache (
-    val countAtWord: Int,
-    val countAtByteZero: Int,
-    val countAtByteOne: Int,
-    val countAtByteTwo: Int,
-    val countAtByteThree: Int
+    var countAtStepEdge: Int,
+    var countAtByte: Vector[Int]
 )
 
-class Rank(bitArg: BitVector, bitStepSizeArg: Int = 512) {
+class Rank(bitArg: BitVector, bitStepSizeArg: Int = 4) {
   val bits: BitVector = bitArg
   val bitStepSize: Int = bitStepSizeArg
-//   val cache: Vector[RankCache] = Rank.populateCache(bits)
+  val cache: Vector[RankCache] = Rank.populateCache(bits, bitStepSizeArg)
 }
 
 object Rank {
@@ -30,10 +27,63 @@ object Rank {
         }
         return result
     }
-    // def populateCache(bits: BitVector): Vector[RankCache] = {
-    //     val bits_count = bits.length
-    //     val bytes_count = java.lang.Math.ceil(bits_count / 8.0)
-    //     var result: Vector[RankCache] = Vector()
-    //     return result
-    // }
+    def populateCache(bits: BitVector, stepSize: Int): Vector[RankCache] = {
+        val bitsCount = bits.length
+        val stepCount = java.lang.Math.ceil(bitsCount.toDouble / (8.0 * stepSize.toDouble)).toInt
+        var result: Vector[RankCache] = Vector()
+        var runningBitCount: Int = 0
+
+        for (index <- 0 until stepCount) {
+            printf("Index: %d\n", index)
+            var stepTotal: Int = 0
+            var temp: RankCache = new RankCache(runningBitCount, Vector())
+            val bytesLeft = ((bitsCount / 8.0) - (index * stepSize)).toInt
+            val iteratorBoundary = java.lang.Math.min(stepSize, bytesLeft).toInt
+            printf("BytesLeft: %d, iteratorBoundary: %d\n", bytesLeft, iteratorBoundary)
+            if (iteratorBoundary == 0) {
+                val bytesCounted = bitsCount / 8
+                val bitsToGo: Int = (bitsCount % 8).toInt
+                var byteCount = 0
+                printf("    Found bit fragments. Bytes counted: %d, bitsToGo: %d\n", bytesCounted, bitsToGo)
+                for (bitFragment <- 0 until bitsToGo) {
+                    printf("      bitFragment: %d\n", bitFragment)
+                    val bitIndex = (index * stepSize * 8) + bitFragment
+                    val bitIsHigh = bits.get(bitIndex)
+                    if (bitIsHigh) {
+                        byteCount += 1
+                    }
+                }
+                stepTotal += byteCount
+                temp.countAtByte = temp.countAtByte :+ byteCount
+            } else {
+                for (byteIndex <- 0 until iteratorBoundary) {
+                    printf("  byteIndex: %d\n", byteIndex)
+                    val arrayIndex: Long = ((index * stepSize) + byteIndex).toLong
+                    var byteCount = 0
+                    if (((arrayIndex + 1) * 8) > bitsCount) {
+                        val bytesCounted = bitsCount / 8
+                        val bitsToGo: Int = (bitsCount % 8).toInt
+                        printf("    Found bit fragments. Bytes counted: %d, bitsToGo: %d\n", bytesCounted, bitsToGo)
+                        for (bitFragment <- 0 until bitsToGo) {
+                            printf("      bitFragment: %d\n", bitFragment)
+                            val bitIndex = (arrayIndex * 8) + bitFragment
+                                val bitIsHigh = bits.get(bitIndex)
+                            if (bitIsHigh) {
+                                byteCount += 1
+                            }
+                        }
+                    } else {
+                        byteCount = countOnesInByte(bits.getByte(arrayIndex))
+                    }
+                        stepTotal += byteCount
+                    temp.countAtByte = temp.countAtByte :+ byteCount
+                }
+            }
+            result = result :+ temp
+            runningBitCount += stepTotal
+        }
+        
+        printf("Done.\n\n")
+        return result
+    }
 }
