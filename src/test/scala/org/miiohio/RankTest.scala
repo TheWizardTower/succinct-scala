6441f44e5ff35f00ff7960c536990b25e27122e4
Adam McCullough <merlinfmct87@gmail.com>
2020-07-25 23:53:09 -0700

    Scalafmt again.

--- src/test/scala/org/miiohio/RankTest.scala
+++ src/test/scala/org/miiohio/RankTest.scala
@@ -17,26 +17,63 @@ class RankTest extends AnyFlatSpec {
   }
 
   "countOnesInByteUpToIndex" should "Stop at the correct place." in {
-      assert(Rank.countOnesInByteUpToIndex(127, 0) == 0)
-      assert(Rank.countOnesInByteUpToIndex(-128, 0) == 1)
-      assert(Rank.countOnesInByteUpToIndex(-1, 0) == 1)
-      assert(Rank.countOnesInByteUpToIndex(-1, 1) == 2)
+    assert(Rank.countOnesInByteUpToIndex(127, 0) == 0)
+    assert(Rank.countOnesInByteUpToIndex(-128, 0) == 1)
+    assert(Rank.countOnesInByteUpToIndex(-1, 0) == 1)
+    assert(Rank.countOnesInByteUpToIndex(-1, 1) == 2)
   }
 
   "populateCache" should "Count correctly." in {
-      assert(Rank.populateCache(hex"00000000".toBitVector, 4) == Vector(RankCache(0, Vector(0,0,0,0))))
-      assert(Rank.populateCache(hex"0000000000000000".toBitVector, 4) == Vector(RankCache(0, Vector(0,0,0,0)), RankCache(0, Vector(0,0,0,0))))
-      assert(Rank.populateCache(hex"deadbeef".toBitVector, 4) == Vector(RankCache(0, Vector(6, 11, 17, 24))))
-      assert(Rank.populateCache(hex"deadbabe".toBitVector, 4) == Vector(RankCache(0, Vector(6,11,16,22))))
-      assert(Rank.populateCache(hex"deadbeefdeadbabe".toBitVector, 4) == Vector(RankCache(0, Vector(6,11,17,24)), RankCache(24, Vector(6,11,16,22))))
-      assert(Rank.populateCache(hex"0000".toBitVector, 4) == Vector(RankCache(0, Vector(0,0))))
-      assert(Rank.populateCache(hex"00FF".toBitVector, 4) == Vector(RankCache(0, Vector(0,8))))
+    assert(
+      Rank.populateCache(hex"00000000".toBitVector, 4) == Vector(
+        RankCache(0, Vector(0, 0, 0, 0))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"0000000000000000".toBitVector, 4) == Vector(
+        RankCache(0, Vector(0, 0, 0, 0)),
+        RankCache(0, Vector(0, 0, 0, 0))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"deadbeef".toBitVector, 4) == Vector(
+        RankCache(0, Vector(6, 11, 17, 24))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"deadbabe".toBitVector, 4) == Vector(
+        RankCache(0, Vector(6, 11, 16, 22))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"deadbeefdeadbabe".toBitVector, 4) == Vector(
+        RankCache(0, Vector(6, 11, 17, 24)),
+        RankCache(24, Vector(6, 11, 16, 22))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"0000".toBitVector, 4) == Vector(
+        RankCache(0, Vector(0, 0))
+      )
+    )
+    assert(
+      Rank.populateCache(hex"00FF".toBitVector, 4) == Vector(
+        RankCache(0, Vector(0, 8))
+      )
+    )
   }
 
   "populateCache" should "Handle not-byte-aligned arrays." in {
-      assert(Rank.populateCache(bin"00", 4) == Vector(RankCache(0, Vector(0))))
-      assert(Rank.populateCache(bin"11", 4) == Vector(RankCache(0, Vector(2))))
-      assert(Rank.populateCache(BitVector.concat(Vector[BitVector](hex"00000000".toBitVector, (bin"11"))), 4) == Vector(RankCache(0, Vector(0,0,0,0,2))))
+    assert(Rank.populateCache(bin"00", 4) == Vector(RankCache(0, Vector(0))))
+    assert(Rank.populateCache(bin"11", 4) == Vector(RankCache(0, Vector(2))))
+    assert(
+      Rank.populateCache(
+        BitVector.concat(
+          Vector[BitVector](hex"00000000".toBitVector, (bin"11"))
+        ),
+        4
+      ) == Vector(RankCache(0, Vector(0, 0, 0, 0, 2)))
+    )
   }
 
 }
